39f12a755f8e3e51e9827d817825b05f425cff6d
Nayla Hilana da Silva <hilanacs@outlook.com>
2024-08-10 11:10:51 -0300

    chore: ajuste na configuração do Jest para buscar testes fora da pasta src

--- jest.config.ts
+++ jest.config.ts
@@ -1,14 +1,17 @@
-module.exports = {
-    moduleFileExtensions: ['js', 'json', 'ts'],
-    rootDir: 'src',
-    testRegex: '.*\\.spec\\.ts$',
-    transform: {
-      '^.+\\.(t|j)s$': 'ts-jest',
-    },
-    collectCoverageFrom: [
-      '**/*.(t|j)s',
-    ],
-    coverageDirectory: '../coverage',
-    testEnvironment: 'node',
-  };
-  
+import { Config } from '@jest/types';
+
+const config: Config.InitialOptions = {
+  moduleFileExtensions: ['js', 'json', 'ts'],
+  rootDir: '.',
+  transform: {
+    '^.+\\.(t|j)s$': 'ts-jest',
+  },
+  collectCoverageFrom: [
+    '**/*.(t|j)s',
+  ],
+  coverageDirectory: '../coverage',
+  testEnvironment: 'node',
+  testMatch: ['**/test/**/*.spec.ts'], // Procurar na pasta "test" fora de "src"
+};
+
+export default config;
