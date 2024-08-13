5bc2d70695a36d908bc7d393bb35d123d39a58f3
Nayla Hilana da Silva <hilanacs@outlook.com>
2024-08-13 18:08:37 -0300

    feat(app) adiciona GerenteModule ao AppModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { ContaController } from './controllers/conta.controller';
 import { ContaService } from './services/conta.service';
+import { ClienteModule } from './modules/cliente.module';
+import { GerenteModule } from './modules/gerente.module';
 
 @Module({
-  imports: [],
+  imports: [ClienteModule, GerenteModule],
   controllers: [ContaController],
   providers: [ContaService],
 })
